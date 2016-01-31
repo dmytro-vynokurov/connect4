8118d1c3419c4e56d337f64216875ef2b59fa897
dmytro <dvinokurov777@gmail.com>
2016-01-31 18:32:04 +0200

    extended application description in javadoc

--- src/main/java/com/dvynokurov/ApplicationRunner.java
+++ src/main/java/com/dvynokurov/ApplicationRunner.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * This is a entry point of the backend part of the "Connect 4" game
  *
  * Game description can be found here: https://en.wikipedia.org/wiki/Connect_Four
+ *
+ * Application has included a simple AI player that does it's turn right after human player.
  */
 @SpringBootApplication
 public class ApplicationRunner {
